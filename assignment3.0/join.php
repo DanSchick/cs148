ed1ae9dd118f11165678b089e60ca894acee76fd
Daniel J Schick <dschick@silk1.uvm.edu>
2015-10-07 12:05:52 -0400

    added join page

---
+++ assignment3.0/join.php
@@ -0,0 +1,20 @@
+<?php
+include "lib/constants.php";
+require_once('lib/custom-functions.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>List of Queries</title>
+<?php include "top.php"; ?>
+<br>
+<p>You can click on these to execute a bunch of fun queries!</p>
+<a href="q01.php">q01</a>
+<a href="q02.php">q02</a>
+<a href="q03.php">q03</a>
+<a href="q04.php">q04</a>
+<a href="q05.php">q05</a>
+<a href="q06.php">q06</a>
+
+</body>
+</html>
