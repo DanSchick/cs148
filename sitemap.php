c6a844866de6285ff8560258fc9d6d846b1358ac
Danny Schick <dschick@uvm.edu>
2015-10-27 21:18:50 -0400

    added sitemap

---
+++ sitemap.php
@@ -0,0 +1,6 @@
+<?php
+print '<head><title>Sitemap</title></head>';
+print '<body><p><a href=assignment1.0/index.php>Assignment1.0</a></p>';
+print '<p><a href=assignment2.0/index.php>Assignment2.0</a></p>';
+print '<p><a href=assignment3.0/index.php>Assignment3.0</a></p>';
+print '<p><a href=advising/index.php>Advising</a></p>';
